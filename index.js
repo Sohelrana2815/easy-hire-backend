87a19c7c58309a358d331bf325484e1538e4827c
Sohelrana2815 <sohel152302@gmail.com>
2024-12-13 08:11:06 +0600

    create patch method to update the my posted jobs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5q2fm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,13 +27,67 @@ async function run() {
     // await client.connect();
 
     const jobsCollection = client.db("EASY_HIRE_DB").collection("jobs");
-
-    app.post("/jobs", async (req, res) => {
-      const job = req.body;
-      const result = await jobsCollection.insertOne(job);
+    const myPostedJobsCollection = client
+      .db("EASY_HIRE_DB")
+      .collection("myPostedJobs");
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
       res.send(result);
     });
 
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await jobsCollection.findOne(filter);
+      res.send(result);
+    });
+
+    app.post("/myPostedJobs", async (req, res) => {
+      const job = req.body;
+      console.log(job);
+
+      const result = await myPostedJobsCollection.insertOne(job);
+      res.send(result);
+    });
+
+    app.get("/myPostedJobs", async (req, res) => {
+      const result = await myPostedJobsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/myPostedJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await myPostedJobsCollection.findOne(filter);
+      res.send(result);
+    });
+    app.patch("/myPostedJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const myPostedJob = req.body;
+      console.log(myPostedJob, id);
+
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          jobTitle: myPostedJob.jobTitle,
+          deadline: myPostedJob.deadline,
+          description: myPostedJob.description,
+          category: myPostedJob.category,
+          minimumPrice: myPostedJob.minimumPrice,
+          maximumPrice: myPostedJob.maximumPrice,
+        },
+      };
+
+      const result = await myPostedJobsCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
+
+    app.delete("/myPostedJobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await myPostedJobsCollection.deleteOne(filter);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
